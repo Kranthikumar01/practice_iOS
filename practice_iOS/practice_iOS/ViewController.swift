bd367e5bc7cc62c7202000f4a3e3a53ac15cb98e
Kranthi Kumar <kranthikumar0106@gmail.com>
2022-12-04 18:28:57 +0530

    camera functionalities added

--- practice_iOS/practice_iOS/ViewController.swift
+++ practice_iOS/practice_iOS/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -30,18 +31,49 @@ class ViewController: UIViewController {
     
 
     @IBAction func barCodeBtn(_ sender: Any) {
-        self.scanView = scannerView(frame: CGRect(x: self.view.frame.width/9, y: self.view.frame.height/8, width: self.view.frame.width*0.78, height: self.view.frame.height*0.8))
+        
+        self.scannerAction()
+        
+    }
+    func scannerAction() {
+        self.view.endEditing(true)
+        if AVCaptureDevice.authorizationStatus(for: AVMediaType(rawValue: convertFromAVMediaType(AVMediaType.video))) ==  AVAuthorizationStatus.authorized {
+            self.startScanning()
+        } else {
+            AVCaptureDevice.requestAccess(for: AVMediaType(rawValue: convertFromAVMediaType(AVMediaType.video)), completionHandler: { (granted :Bool) -> Void in
+                DispatchQueue.main.async {
+                    if granted == true {
+                        self.startScanning()
+                    } else {
+                        self.showAlert(message: "REJECTED_CAMERA_SUPPORT")
+                    }
+                }
+            })
+        }
+    }
+    func startScanning() {
+        self.scanView = UINib(nibName: "scannerView", bundle: nil).instantiate(withOwner: self, options: nil)[0] as? scannerView
+        self.scanView.frame = CGRect(x: 10, y: self.view.frame.height/8.5, width: self.view.frame.width-20, height: self.view.frame.height*0.75)
         self.view.addSubview(scanView)
         self.scanView.setupView()
-        print("scanner")
-        
+        self.scanView.setScanView()
     }
     
     @IBAction func bioMetricBtn(_ sender: Any) {
-        print("there is something")
+        
         scanView.alpha = 0
         self.scanView.removeFromSuperview()
     }
-    
+    func showAlert(title:String = "Alert", message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
+func convertFromAVMediaType(_ input: AVMediaType) -> String {
+    return input.rawValue
+}
+func convertFromAVLayerVideoGravity(_ input: AVLayerVideoGravity) -> String {
+    return input.rawValue
+}
--- practice_iOS/practice_iOS/scannerView.swift
+++ practice_iOS/practice_iOS/scannerView.swift
@@ -6,27 +6,54 @@
 //
 
 import UIKit
+import AVFoundation
 
-class scannerView: UIView {
+class scannerView: UIView, AVCaptureMetadataOutputObjectsDelegate {
+    var ratio:CGFloat = 0.208
+    var ratioHeight:CGFloat!
+    let session = AVCaptureSession()
+    var previewLayer : AVCaptureVideoPreviewLayer?
 
     @IBOutlet weak var scanArea: UIView!
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    
     override func awakeFromNib() {
-        scanArea.backgroundColor = .clear
+        self.backgroundColor = .red
     }
     func setupView() {
         self.backgroundColor = .gray
         self.layer.cornerRadius = 14
         self.layer.shadowOffset = CGSize(width: 4, height: 8)
         self.layer.shadowColor = CGColor.init(red: 99, green: 78, blue: 43, alpha: 0.4)
-        
-        
     }
-
+    func addPreviewLayer() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer?.videoGravity = AVLayerVideoGravity(rawValue: convertFromAVLayerVideoGravity(AVLayerVideoGravity.resizeAspectFill))
+        previewLayer?.bounds = self.bounds
+        previewLayer?.position = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+        self.layer.addSublayer(previewLayer!)
+    }
+    open func setScanView() {
+        ratioHeight = self.frame.height * ratio
+        
+        let inputDevice:AVCaptureDeviceInput!
+        do {
+            if let captureDevice = AVCaptureDevice.default(for: AVMediaType(rawValue: convertFromAVMediaType(AVMediaType.video))) {
+                inputDevice =  try AVCaptureDeviceInput(device: captureDevice)  //AVCaptureDeviceInput(device: captureDevice, error: &error)
+                if let inp = inputDevice {
+                    session.addInput(inp)
+                }
+            }
+        } catch {
+            print("error")
+        }
+        
+        addPreviewLayer()
+        
+        /* Check for metadata */
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        output.metadataObjectTypes = output.availableMetadataObjectTypes
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        session.startRunning()
+    }
 }
