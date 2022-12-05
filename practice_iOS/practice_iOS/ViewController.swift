448a2c9417a99adc5b3d6c858f266ed40c9e1b5e
Kranthi Kumar <kranthikumar0106@gmail.com>
2022-12-05 14:06:10 +0530

    biometric added

--- practice_iOS/practice_iOS/ViewController.swift
+++ practice_iOS/practice_iOS/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AVFoundation
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
@@ -55,20 +56,52 @@ class ViewController: UIViewController {
         self.scanView = UINib(nibName: "scannerView", bundle: nil).instantiate(withOwner: self, options: nil)[0] as? scannerView
         self.scanView.frame = CGRect(x: 10, y: self.view.frame.height/8.5, width: self.view.frame.width-20, height: self.view.frame.height*0.75)
         self.view.addSubview(scanView)
+        self.scanView.delegate = self
         self.scanView.setupView()
         self.scanView.setScanView()
     }
     
     @IBAction func bioMetricBtn(_ sender: Any) {
+        let context = LAContext()
+            var error: NSError?
+
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Identify yourself!"
+
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                    [weak self] success, authenticationError in
+
+                    DispatchQueue.main.async {
+                        if success {
+                            self?.showAlert(message: "unlocked")
+                        } else {
+                            // error
+                        }
+                    }
+                }
+            } else {
+                // no biometry
+            }
         
-        scanView.alpha = 0
-        self.scanView.removeFromSuperview()
+        
     }
     func showAlert(title:String = "Alert", message:String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
+}
+
+extension ViewController: scanViewDelegate {
+    func decodedOutput(_ outValue: String) {
+        self.showAlert(message: outValue)
+    }
+    
+    func dismissView() {
+        self.scanView.removeFromSuperview()
+    }
+    
+    
 }
 
 func convertFromAVMediaType(_ input: AVMediaType) -> String {
--- practice_iOS/practice_iOS/scannerView.swift
+++ practice_iOS/practice_iOS/scannerView.swift
@@ -8,11 +8,19 @@
 import UIKit
 import AVFoundation
 
+protocol scanViewDelegate {
+    func dismissView()
+    func decodedOutput(_ outValue: String)
+}
+
 class scannerView: UIView, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var delegate: scanViewDelegate!
     var ratio:CGFloat = 0.208
     var ratioHeight:CGFloat!
     let session = AVCaptureSession()
     var previewLayer : AVCaptureVideoPreviewLayer?
+    var scanningLine = UIView()
 
     @IBOutlet weak var scanArea: UIView!
     
@@ -30,6 +38,7 @@ class scannerView: UIView, AVCaptureMetadataOutputObjectsDelegate {
         previewLayer?.videoGravity = AVLayerVideoGravity(rawValue: convertFromAVLayerVideoGravity(AVLayerVideoGravity.resizeAspectFill))
         previewLayer?.bounds = self.bounds
         previewLayer?.position = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+        AVCaptureVideoPreviewLayer().cornerRadius = 14
         self.layer.addSublayer(previewLayer!)
     }
     open func setScanView() {
@@ -38,7 +47,7 @@ class scannerView: UIView, AVCaptureMetadataOutputObjectsDelegate {
         let inputDevice:AVCaptureDeviceInput!
         do {
             if let captureDevice = AVCaptureDevice.default(for: AVMediaType(rawValue: convertFromAVMediaType(AVMediaType.video))) {
-                inputDevice =  try AVCaptureDeviceInput(device: captureDevice)  //AVCaptureDeviceInput(device: captureDevice, error: &error)
+                inputDevice =  try AVCaptureDeviceInput(device: captureDevice)
                 if let inp = inputDevice {
                     session.addInput(inp)
                 }
@@ -55,5 +64,55 @@ class scannerView: UIView, AVCaptureMetadataOutputObjectsDelegate {
         output.metadataObjectTypes = output.availableMetadataObjectTypes
         output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
         session.startRunning()
+        
+        
+        let dismissButton = UIButton()
+        dismissButton.frame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        dismissButton.setImage(UIImage(named: "close"), for: UIControl.State())
+        dismissButton.addTarget(self, action: #selector(scannerView.dismissScanView), for: UIControl.Event.touchUpInside)
+        self.addSubview(dismissButton)
+        
+        let scannerLayer = UIImageView(frame: self.frame)
+        scannerLayer.image = UIImage(named: "scanner")
+        self.addSubview(scannerLayer)
+        
+        self.scanningLine.frame = CGRect(x: 0, y: 0, width: self.frame.width - 70, height: 2)
+        self.scanningLine.center = CGPoint(x: self.center.x-10, y: self.center.y - 160)
+        self.scanningLine.backgroundColor = .white//UIImage(named: "scanner_line")
+        self.addSubview(self.scanningLine)
+        
+        UIView.animate(withDuration: 1.3, delay:0, options: [.repeat, .autoreverse], animations: {
+            self.scanningLine.frame = CGRect(x: self.scanningLine.frame.origin.x, y: self.scanningLine.frame.origin.y + 180, width: self.scanningLine.frame.width, height: self.scanningLine.frame.height)
+            }, completion: nil)
     }
+    
+    @objc func dismissScanView() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        session.stopRunning()
+        self.removeFromSuperview()
+        delegate.dismissView()
+    }
+    
+    open func metadataOutput(_ captureOutput: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        self.endEditing(true)
+        for data in metadataObjects {
+            let metaData = data
+            let transformed = previewLayer?.transformedMetadataObject(for: metaData) as? AVMetadataMachineReadableCodeObject
+            if let unwraped = transformed {
+                if let decodeValue = unwraped.stringValue {
+                    print("======>>>   ",decodeValue)
+                    delegate.decodedOutput(decodeValue)
+                    self.scanningLine.layer.removeAllAnimations()
+                    self.session.stopRunning()
+                    let systemSoundID: SystemSoundID = 1114
+                    AudioServicesPlaySystemSound (systemSoundID)
+                    AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+                    Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(scannerView.dismissScanView), userInfo: nil, repeats: false)
+                    return
+                }
+            }
+        }
+    }
+
 }
